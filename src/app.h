d609e551b16aca5d023a2fe7fc4c66ab4823a01c
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-09-19 02:43:19 +0300

    Update app.h

--- src/app.h
+++ src/app.h
@@ -42,26 +42,27 @@ struct ctx {
 
 extern struct ctx G_CTX;
 
-void ctx_init(struct ctx *c, u64 eboot_base, u64 dlsym_addr, struct ext_args *ext);
-int  ctx_video_up(struct ctx *c, u64 eboot_base);
-void ctx_audio_up(struct ctx *c);
-void ctx_pad_up(struct ctx *c);
-void ctx_cleanup(struct ctx *c);
+void ctx_init      (struct ctx *c, u64 eboot_base, u64 dlsym_addr, struct ext_args *ext);
+int  ctx_video_up  (struct ctx *c, u64 eboot_base);
+void ctx_audio_up  (struct ctx *c);
+void ctx_pad_up    (struct ctx *c);
+void ctx_pad_retry (struct ctx *c);
+void ctx_cleanup   (struct ctx *c);
 
-void ulog(struct ctx *c, const char *msg);
-void ulog_num(struct ctx *c, const char *prefix, u64 v);
+void ulog     (struct ctx *c, const char *msg);
+void ulog_num (struct ctx *c, const char *prefix, u64 v);
 
-u32  pad_raw(struct ctx *c);
-int  pad_set_lightbar(struct ctx *c, u8 r, u8 g, u8 b);
-int  pad_set_vibration(struct ctx *c, u8 large, u8 small);
-int  pad_set_trigger(struct ctx *c, int mode, int trig, int p1, int p2, int p3);
+u32  pad_raw              (struct ctx *c);
+int  pad_set_lightbar     (struct ctx *c, u8 r, u8 g, u8 b);
+int  pad_set_vibration    (struct ctx *c, u8 large, u8 small);
+int  pad_set_trigger      (struct ctx *c, int mode, int trig, int p1, int p2, int p3);
 
-void video_flip(struct ctx *c, int wait_vsync);
+void video_flip (struct ctx *c, int wait_vsync);
 
-u64  get_uptime_ms(struct ctx *c);
-u64  get_dm_size(struct ctx *c);
+u64  get_uptime_ms (struct ctx *c);
+u64  get_dm_size   (struct ctx *c);
 
 /* audio: play a square wave tone for `ms` milliseconds (blocking) */
-void audio_tone(struct ctx *c, int freq, int ms);
+void audio_tone (struct ctx *c, int freq, int ms);
 
 #endif
